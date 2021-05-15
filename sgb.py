c9167054f259c67565fcedf7e4a9e62793d16467
Fatih Kartal <fatih.kartal@mysugr.com>
2021-05-15 17:50:23 +0200

    Change working dir in executables

--- sgb.py
+++ sgb.py
@@ -6,7 +6,12 @@ This is an attempt to recreate the first level of
 Super Mario Bros for the NES.
 """
 
+import os
 import sys
+
+if getattr(sys, 'frozen', False):
+    os.chdir(os.path.join(sys._MEIPASS)) # pyinstaller executables use this hidden folder as their working directory
+
 import pygame as pg
 from data.main import main
 import cProfile
